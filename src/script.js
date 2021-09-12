3fde9d3693bc3761c061c84ae170c33021f4023d
mpierre24 <pierremlox@gmail.com>
2021-09-12 15:15:20 -0400

    modified src/script.js

--- src/script.js
+++ src/script.js
@@ -2,7 +2,7 @@ const startButton = document.getElementById('start-btn')
 const nextButton = document.getElementById('next-btn')
 const questionContainerElement = document.getElementById('question-container')
 const questionElement = document.getElementById('question')
-const answerButtonsElement = document.getElementById(answer - buttons)
+const answerButtonElement = document.getElementById(answer - buttons)
 
 
 let shuffledQuestions, currentQuestionsIndex
